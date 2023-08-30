ca234b3f60dbf6ade2b643f2f26a7494dbf0b125
Maninder Pahal <mpahal123@gmail.com>
2023-08-29 22:18:53 -0700

    recomitting

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
-const root = document.getElementById('root');
-root.textContent = 'js file works jsx doesnt'
