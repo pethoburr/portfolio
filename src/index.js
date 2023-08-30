a168856f9334d2b22e0b6cb18265e9a1fccaabca
Maninder Pahal <mpahal123@gmail.com>
2023-08-29 21:51:18 -0700

    testing too see if js index file works

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = document.getElementById('root');
+root.textContent = 'js file works jsx doesnt'
