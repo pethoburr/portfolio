15dc414258f4b91e2fb03050c815ada7b2888d73
Maninder Pahal <mpahal123@gmail.com>
2023-09-05 21:08:22 -0700

    edited vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
-  return {
-    define: {
-        'process.env': {},
-    },
-    build: {
-      outDir: 'build',
-    },
+export default defineConfig({
     base: '/portfolio/',
-    plugins: [react()],
-    
-  };
+    plugins: [react()], 
 });
